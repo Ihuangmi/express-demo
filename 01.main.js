c58d1b3b9928398ca136689224b91c28aa88193d
huangmi <huangmi@newrank.cn>
2023-03-24 16:51:38 +0800

    feat: 优化

--- 01.main.js
+++ 01.main.js
@@ -2,13 +2,11 @@ const express = require("express");
 const app = express();
 
 // 导入路由模块
-const router = require("./router");
+const router = require("./01.router");
 
 // app.use() 函数的作用，就是用来注册全局中间件
 // 注册路由模块，并添加统一的访问前缀 /api
 app.use("/api", router);
-
-
 
 app.listen(80, () => {
   console.log("server running at http://127.0.0.1");
